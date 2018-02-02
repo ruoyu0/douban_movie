0da7e4cb675368e531cdab29d6119cd0a6320026
zhuyuan <849434163@qq.com>
2018-02-02 10:53:48 +0800

    添加redis相关的docker操作

--- common/init.sh
+++ common/init.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+# docker 查询mongo的image
+docker search mongo
+
 # docker下载mongo的image
 docker pull mongo
 
 # docker运行mongo容器
-docker run -p 27017:27017 -d -v /data/db:/data/db --name mongodb mongo --storageEngine wiredTiger
+docker run -p 127.0.0.1:27017:27017 -d -v /data/db:/data/db --name mongodb mongo --storageEngine wiredTiger
 
-#
+# redis 相关
+docker search redis
+docker pull redis
+docker run -p 127.0.0.1:6379:6379 --name redis redis
